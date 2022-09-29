b53ddbae606c30fb133e45ff3513338e6fb67478
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2022-09-29 13:35:44 +0100

    chore: modified env to include the auth0 variables

--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -1,14 +1,14 @@
-/* @TODO replace with your variables
+/* @DONE replace with your variables
  * ensure all variables on this page match your project
  */
 
 export const environment = {
   production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  apiServerUrl: "http://127.0.0.1:5000", // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
-  }
+    url: "dev-ya94ppfc.us", // the auth0 domain prefix
+    audience: "drinks", // the audience set for the auth0 app
+    clientId: "Vna6pHZHZ9P1dvOeq6POJ2WB8lBsfY8G", // the client id generated for the auth0 app
+    callbackURL: "http://localhost:8100", // the base url of the running ionic application.
+  },
 };
